e129dc579e704f7f987769cc814cc65dedb4a621
ariqrm <ariqrm@gmail.com>
2019-08-31 12:23:23 +0700

    reconfig

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,17 +2,17 @@ require('dotenv').config()
 
 const mysql = require('mysql')
 
-const conn = mysql.createConnection({
-  host: process.env.DB_HOST || "remotemysql.com",
-  user: process.env.DB_USER || "TQcD990PhV",
-  password: process.env.DB_PASSWORD || "tFU1324eFE",
-  database: process.env.DB_NAME || "TQcD990PhV"
-})
 // const conn = mysql.createConnection({
-//   host: "remotemysql.com",
-//   user: "TQcD990PhV",
-//   password: "tFU1324eFE",
-//   database: "TQcD990PhV"
+//   host: process.env.DB_HOST || "remotemysql.com",
+//   user: process.env.DB_USER || "TQcD990PhV",
+//   password: process.env.DB_PASSWORD || "tFU1324eFE",
+//   database: process.env.DB_NAME || "TQcD990PhV"
 // })
+const conn = mysql.createConnection({
+  host: "remotemysql.com",
+  user: "TQcD990PhV",
+  password: "tFU1324eFE",
+  database: "TQcD990PhV"
+})
 
 module.exports = conn
